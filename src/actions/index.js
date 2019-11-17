321718e982de0d89fe44dd0680d103d8776420c0
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-11-17 12:28:21 -0300

    279. Fetching the Deletion Stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -38,10 +38,10 @@ export const fetchStreams = () => async dispatch => {
     dispatch({ type: FETCH_STREAMS, payload: response.data});
 };
 
-export const fetchStream = id => async dispatch => {
+export const fetchStream = (id) => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
-
-    dispatch({ type: FETCH_STREAM, payload: response.data});
+  
+    dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formvalues) => async dispatch => {
